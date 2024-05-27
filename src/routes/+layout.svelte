c651f8e29355de218a106a821761c4b474d0c57e
TrashHat <jcdolojan@up.edu.ph>
2024-05-27 22:39:45 +0800

    new file:   .eslintrc.cjs 	new file:   .gitignore 	new file:   .npmrc 	new file:   .prettierignore 	new file:   .prettierrc 	new file:   .vscode/settings.json 	new file:   README.md 	new file:   my-custom-theme.ts 	new file:   package-lock.json 	new file:   package.json 	new file:   playwright.config.ts 	new file:   pnpm-lock.yaml 	new file:   postcss.config.cjs 	new file:   src/app.d.ts 	new file:   src/app.html 	new file:   src/app.postcss 	new file:   src/index.test.ts 	new file:   src/lib/index.ts 	new file:   src/routes/+layout.svelte 	new file:   src/routes/+page.svelte 	new file:   static/favicon.png 	new file:   svelte.config.js 	new file:   tailwind.config.ts 	new file:   tests/test.ts 	new file:   tsconfig.json 	new file:   vite.config.ts

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,60 @@
+<script lang ="ts">
+	import '../app.postcss';
+
+	// Highlight JS
+	import hljs from 'highlight.js/lib/core';
+	import 'highlight.js/styles/github-dark.css';
+	import { storeHighlightJs } from '@skeletonlabs/skeleton';
+	import xml from 'highlight.js/lib/languages/xml'; // for HTML
+	import css from 'highlight.js/lib/languages/css';
+	import javascript from 'highlight.js/lib/languages/javascript';
+	import typescript from 'highlight.js/lib/languages/typescript';
+
+	hljs.registerLanguage('xml', xml); // for HTML
+	hljs.registerLanguage('css', css);
+	hljs.registerLanguage('javascript', javascript);
+	hljs.registerLanguage('typescript', typescript);
+	storeHighlightJs.set(hljs);
+
+	import {AppShell, AppBar, LightSwitch} from '@skeletonlabs/skeleton';
+
+</script>
+
+<AppShell regionPage="relative" slotPageHeader="sticky top-0 z-10">
+	<svelte:fragment slot="header">
+		<AppBar slotDefault="place-self-center" slotTrail="place-content-end gap-4'" background="bg-error-900 dark:bg-zinc-900">
+			<svelte:fragment slot="lead">
+				<a href="#Home" class="text-white hover:ease-in duration-300 hover:opacity-50"><img src="/" alt="DataPuti"></a>
+			</svelte:fragment>
+			<nav class="gap-11">
+				<a href="#Overview" class="text-white text-2xl hover:text-3xl hover:text-primary-500 hover:ease-in-out duration-300 hover:font-bold">Overview</a> <p class="text-white">|</p> 
+				<a href="#Data" class="text-white text-2xl hover:text-3xl hover:text-primary-500 hover:ease-in-out duration-300 hover:font-bold">Data</a> <p class="text-white">|</p> 
+				<a href="#Methods" class="text-white text-2xl hover:text-3xl hover:text-primary-500 hover:ease-in-out duration-300 hover:font-bold">Methods</a> <p class="text-white">|</p> 
+				<a href="#Results" class="text-white text-2xl hover:text-3xl hover:text-primary-500 hover:ease-in-out duration-300 hover:font-bold">Results</a> <p class="text-white">|</p> 
+				<a href="#Team" class="text-white text-2xl hover:text-3xl hover:text-primary-500 hover:ease-in-out duration-300 hover:font-bold">Team</a>
+			</nav>
+			<svelte:fragment slot="trail">
+				<LightSwitch />
+			</svelte:fragment>
+		</AppBar>
+	</svelte:fragment>
+	<!-- (sidebarLeft) -->
+	<!-- (sidebarRight) -->
+	<!--(pageHeader)-->
+	<!-- Router Slot -->
+	<div class="container p-10 mx-auto">
+		<slot />
+	</div>
+	<!-- ---- / ---- -->
+	<svelte:fragment slot="pageFooter">
+		<AppBar background="bg-error-900 dark:bg-zinc-900 text-white">
+			<svelte:fragment slot="lead">
+				<p></p>
+			</svelte:fragment>
+			<h6>
+				Team DataPuti
+			</h6>
+		</AppBar>
+	</svelte:fragment>
+	<!-- (footer) -->
+</AppShell>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,130 @@
+<!-- YOU CAN DELETE EVERYTHING IN THIS PAGE
+
+<div class="container h-full mx-auto flex justify-center items-center">
+	<div class="space-y-5">
+		<h1 class="h1">Let's get cracking bones!</h1>
+		<p>Start by exploring:</p>
+		<ul>
+			<li><code class="code">/src/routes/+layout.svelte</code> - barebones layout</li>
+			<li><code class="code">/src/app.postcss</code> - app wide css</li>
+			<li>
+				<code class="code">/src/routes/+page.svelte</code> - this page, you can replace the contents
+			</li>
+		</ul>
+	</div>
+</div> -->
+
+<!-- Home -->
+<div>
+	<article id="Home" class="wrapper style1">
+		<div class="container">
+			<div class="row">
+				<div class="col-4 col-5-large col-12-medium">
+					<!-- <span class="image fit"><img src="images/pic00.jpg" alt="" /></span> -->
+				</div>
+				<div class="col-8 col-7-large col-12-medium">
+					<header>
+						<h1>Hi. We are <strong>DataPuti</strong>.</h1>
+					</header>
+					<p>Our project aims to identify the interconnectedness between series of political unrest from 2016 to present day. Particularly, our project aims to identify patterns between these events and act as a medium for future researchers aiming to understand the political status of the country and formulate means or methods to improve the country’s current conditions.</p>
+					
+				</div>
+			</div>
+		</div>
+	</article>
+</div>
+
+<br>
+
+<!-- Overview -->
+<div class="block card card-hover p-4 bg-secondary-300 dark:bg-slate-800">
+	<article id="overview" class="wrapper style2">
+		<div class="container">
+			<header>
+				<h2>Here's an overview of our project.</h2>
+				<div class="col-4 col-6-medium col-12-small">
+					<section class="box style1">
+						<h3>Background</h3>
+						<p>In 2021, Freedom House reported a significantly low scores in political rights and civil liberties for the Philippines. This came after  the Duterte administration failed to secure peace treaties with the CPP-NPA and more, declared them as terrorists. Journalists, activities, and opposing political parties also became main targets of harassment and were labelled as communist sympathizers which resulted in increased risks of physical assault from state forces. Furthermore, the administration forced the closure of Islamic and Indigenous schools and the illegal arrests of their students due to allegations as possible recruitment sites for militants. These are among the previous administration's tactics to subvert insurgency which worsened civil conflicts and allowed them to persist until the current administration.</p>
+						<p>References: 
+							<a href="https://freedomhouse.org/country/philippines/freedom-world/2021#PR">[1]</a>
+							<a href="https://www.rappler.com/moveph/lumad-schools-continue-to-suffer-closures-attacks-coronavirus-pandemic/">[2]</a>
+						</p>
+					</section>
+				</div>
+			</header>
+			<div class="row aln-center">
+				<div class="col-4 col-6-medium col-12-small">
+					<section class="box style1">
+						<span class="icon solid featured fa-camera-retro"></span>
+						<h3>Research Questions</h3>
+						<p>1. How interconnected (in terms of geographical and temporal proximity, conflicting sides, cause, etc.) are the conflicts that have occurred since the Duterte administration?</p>
+						<p>2. Do conflicts in the Philippines show patterns (e.g. certain times of the year) on when they are more likely to occur?
+						</p>
+					</section>
+				</div>
+				<div class="col-4 col-6-medium col-12-small">
+					<section class="box style1">
+						<span class="icon featured fa-thumbs-up"></span>
+						<h3>Plan of Action</h3>
+						<p>Our approach uses Data Science to analyze and visualize the factors of interconnection (location, date of occurrence, actors, types of event) of the conflicts since the Duterte administration, as well as examine the timestamps and frequency of these incidents to identify the trends in their occurrence. </p>
+					</section>
+				</div>
+			</div>
+			
+		</div>
+	</article>
+
+	<article id="overview" class="wrapper style2">
+		<div class="container">
+			<header>
+				<h2>Hypothesis and data</h2>
+			</header>
+			<div class="row aln-center">
+				<div class="col-4 col-6-medium col-12-small">
+					<section class="box style1">
+						<h2>Null Hypothesis</h2>
+						<p>The sociopolitical conflicts in the Philippines occur randomly.</p>
+					</section>
+				</div>
+				<div class="col-4 col-6-medium col-12-small">
+					<section class="box style1">
+						<h2>Alternative Hypothesis</h2>
+						<p>The sociopolitical conflicts in the Philippines occur under geographical and historical context.</p>
+					</section>
+				</div>
+			</div>
+		</div>
+		<footer>
+			<a href="https://docs.google.com/document/d/1w8oLWkDG3S4PgmrsU2fRgTXZqCG8nchQ5YBVCGPhkgY/edit?usp=sharing" class="button large scrolly">Guide to the Data Set</a>
+			<p></p>
+			<a href="https://docs.google.com/spreadsheets/d/1h00TUat9JPPCv1T8tWFzQ_t_ZR2ZovRk/edit?usp=drive_link&ouid=108314072179584311538&rtpof=true&sd=true" class="button large scrolly">Data Set</a>
+		</footer>
+	</article>
+</div>
+
+<!-- Data -->
+
+<article id="#Data"></article>
+
+<!-- Methods -->
+
+<article id="#Methods"></article>
+
+<!-- Results -->
+
+<article id="#Results"></article>
+
+<br>
+
+<!-- Team -->
+<div class="block card card-hover p-4 bg-secondary-300 dark:bg-slate-800">
+	<article id="#Team"></article>
+</div>
+
+<!--Reserve for about 
+	<ul style="list-style-type:none; margin-top:-30px;">
+	<li>David Vincent C. Andal, WFZ</li>
+	<li>Julian Andrei C. Dolojan, WFZ</li>
+	<li>Justine Eusebio Soriano, WFZ</li>
+  </ul> -->
